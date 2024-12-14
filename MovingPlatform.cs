c2e393e788aac1e7f04133c9db3ddadd43f6eb21
vasiakyriazopoulou <85514546+vasiakyr@users.noreply.github.com>
2024-12-14 18:50:44 +0200

    Create MovingPlatform.cs

---
+++ MovingPlatform.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public Vector3 finishPos = Vector3.zero;
+    public float speed = 0.5f;
+
+    public Vector3 startPos;
+    private float trackPercent = 0;
+    private int direction = 1;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        trackPercent += direction * speed * Time.deltaTime;
+        float x = (finishPos.x - startPos.x) * trackPercent + startPos.x;
+        float y = (finishPos.y - startPos.y) * trackPercent + startPos.y;
+        transform.position = new Vector3(x, y, startPos.z);
+
+        if (((direction == 1) && (trackPercent > .9f)) || ((direction == -1) && (trackPercent < .1f)))
+        {
+            direction *= -1;
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, finishPos);
+    }
+
+}

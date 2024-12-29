d0decc8187b04c7fc547a5e16df01f917b187bb9
Κυριακή Κ <106349081+knightwisher@users.noreply.github.com>
2024-12-29 20:37:08 +0200

    Update Spike.cs

--- Project2/final/Spike.cs
+++ Project2/final/Spike.cs
@@ -1,3 +1,5 @@
+//για όταν πέφτει πάνω στο box με tag spike
+
 using UnityEngine;
 
 public class Spike : MonoBehaviour
@@ -7,21 +9,21 @@ public class Spike : MonoBehaviour
         // Ελέγχουμε αν το αντικείμενο που συγκρούεται είναι ο παίκτης
         if (collision.gameObject.CompareTag("Player"))
         {
-            Debug.Log("Ο Παίκτης έχασε ζωή! Συγκρούστηκε με τον Εχθρό.");
+            Debug.Log("Player got hit!");
 
             // Αναζητούμε τον Player
             PlatformerPlayer player = collision.gameObject.GetComponent<PlatformerPlayer>();
 
             if (player != null)
             {
-                // Χάνουμε μία ζωή από τον παίκτη
+                // player health -1
                 player.TakeDamage();
 
                 // Αν ο παίκτης έχει 0 ζωές, κάνουμε restart του παιχνιδιού
                 if (player.GetHealth() <= 0)
                 {
                     // Επαναφορά του παίκτη στην αρχική του θέση
-                    player.transform.position = player.StartingPosition(); // Χρησιμοποιούμε τη μέθοδο StartingPosition()
+                    player.transform.position = player.StartingPosition(); 
                     player.ResetHealth(); // Επαναφορά των ζωών του παίκτη
                 }
             }

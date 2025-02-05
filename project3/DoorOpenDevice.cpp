252472f387a9b06cfc7fc49059bd9db73aeed1d0
Κυριακή Κ <106349081+knightwisher@users.noreply.github.com>
2025-02-05 12:23:20 +0200

    Create DoorOpenDevice.cpp

---
+++ project3/DoorOpenDevice.cpp
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpenDevice : MonoBehaviour
+{
+    [SerializeField] Vector3 dPos;
+    private bool open;
+
+    public void Operate()
+    {
+        if (open)
+        {
+            Vector3 pos = transform.position - dPos;
+            transform.position = pos;
+        }
+
+        else
+        {
+            Vector3 pos = transform.position + dPos;
+            transform.position = pos;
+        }
+
+        open = !open;
+    }
+
+    public void Activate()
+    {
+        if (!open)
+        {
+            Vector3 pos = transform.position + dPos;
+            transform.position = pos;
+            open = true;
+        }
+    }
+
+    public void Deactivate()
+    {
+        if (open)
+        {
+            Vector3 pos = transform.position - dPos;
+            transform.position = pos;
+            open = false;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
